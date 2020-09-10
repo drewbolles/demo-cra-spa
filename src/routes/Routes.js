2ff7fe5f6a91294f605b664e8c52b43479b10656
Drew Bolles <drewbolles@gmail.com>
2020-09-09 23:10:47 -0400

    add provider docs

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
+// We're importing this here, but really it could be wherever in your
+// component tree that makes sense. This serves as an example provider that all routes share.
+import { ReposProvider } from '../components/ReposProvider/ReposProvider';
+
 import Home from './Home/Home';
 import About from './About/About';
-import { ReposProvider } from '../components/ReposProvider/ReposProvider';
 
 export default function Routes() {
   return (
